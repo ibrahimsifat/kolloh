2b92770db04807134a8bfebfe83e6357138416a0
ibrahimsifat <ibsifat24@gmail.com>
2023-03-01 21:16:49 +0300

    setup & cretae besic model structure

---
+++ config/plugins.js
@@ -0,0 +1,40 @@
+module.exports = ({ env }) => ({
+  // ...
+  //   upload: {
+  //     config: {
+  //       provider: "cloudinary",
+  //       providerOptions: {
+  //         cloud_name: env("CLOUDINARY_NAME"),
+  //         api_key: env("CLOUDINARY_KEY"),
+  //         api_secret: env("CLOUDINARY_SECRET"),
+  //       },
+  //       actionOptions: {
+  //         upload: {},
+  //         uploadStream: {},
+  //         delete: {},
+  //       },
+  //     },
+  //   },
+  seo: {
+    enabled: true,
+  },
+  "entity-relationship-chart": {
+    enabled: true,
+    config: {
+      // By default all contentTypes and components are included.
+      // To exlclude strapi's internal models, use:
+      exclude: [
+        "strapi::core-store",
+        "webhook",
+        "admin::permission",
+        "admin::user",
+        "admin::role",
+        "admin::api-token",
+        "plugin::upload.file",
+        "plugin::i18n.locale",
+        "plugin::users-permissions.permission",
+        "plugin::users-permissions.role",
+      ],
+    },
+  },
+});
